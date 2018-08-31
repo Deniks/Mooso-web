86095fb13f72ebde441b02ce03f7623c53ee567e
Deniks <rezanovich.denis@gmail.com>
2018-08-31 07:33:24 -0700

    'wtf'

--- soundemotion/settings.py
+++ soundemotion/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'soundemotion.urls'
